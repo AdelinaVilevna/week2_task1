98b73120d51f0024104f5fc9931063b477f228b2
AdelinaVilevna <adelina.abdykanyeva@gmail.com>
2020-11-02 23:15:55 +0600

    week2_task1

---
+++ Abdykanyeva_bonus1.py
@@ -0,0 +1,18 @@
+#1
+
+# lis = [1, 2, 3]
+# n = len (lis) // 2 
+# x = lis[n:] + lis[:n]
+# print(x)
+
+
+# 2
+# lis1 = ["string1", "string2"]
+# lis1.reverse()
+# print(lis1)
+
+# # 3
+# lis = [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+
